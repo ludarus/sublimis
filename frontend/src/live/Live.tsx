c6193e90772a91b8bdeae25e0a676254d66f7589
ludarus <burgermanjoe48@gmail.com>
2026-06-05 23:38:21 -0400

    chat ui dynamic sizing and custom svgs

--- frontend/src/live/Live.tsx
+++ frontend/src/live/Live.tsx
@@ -1,4 +1,5 @@
 import { useParams } from "react-router";
+import { motion } from 'motion/react'
 import { useEffect, useState } from "react"
 
 import useWebSocket from '../ws/useWebSocket.tsx'
@@ -11,6 +12,8 @@ import NavBar from '../home/Nav.tsx'
 
 export default function Live() {
 	const [userInfo, setInfo] = useState<SublimisUser | boolean>(true)
+	const [chatToggled, setChatToggled] = useState<boolean>(true)
+	const [playerCountToggled, setPlayerCountToggled] = useState<boolean>(false)
 
 	let { campaignId } = useParams();
 
@@ -54,38 +57,93 @@ export default function Live() {
 					<NavBar setInfo={setInfo} userInfo={userInfo} />
 				</div>
 				<div id="root-container-live" className="border">
-					<div id="chat-container" className="border border-blue-500">
+					<motion.div id="chat-container" className="border-3 border-blue-500" animate={{ width: chatToggled ? "25vw" : "auto" }} transition={{ duration: 0.2, ease: "easeOut" }}>
 						{/*<h1>{campaignId}</h1>
 											<p>{wsRef.current ? wsRef.current.readyState : ""}</p>*/}
 
-
-						<ul id="messages" className="border border-green-500">
-							{
-								messages.map((msg) => (
-									<li className="message " key={Number(msg.time)}>
-										<div className="message-image">
-											<img className="rounded-full " src={msg.img} />
-										</div>
-										<div className="message-text">
-
-											<div className="message-headings">
-												<h2><b>{msg.name}</b></h2>
-
-												<h3>{new Date(Number(msg.time)).toLocaleTimeString()}</h3>
-											</div>
-											<p className="message-payload">
-												{msg.payload}
-											</p>
-										</div>
-									</li>
-								))
+						<motion.div id="chat-buttons" className="border"
+							animate={{ flexDirection: chatToggled ? "row" : "column" }}
+						>
+							{chatToggled ? (
+								<button id="players-button"  >
+									<svg width="2.307em" height="2em" viewBox="0 0 991 859" fill="none" xmlns="http://www.w3.org/2000/svg" onClick={() => setPlayerCountToggled(!playerCountToggled)}>
+										<path d="M78.2488 325.105L78.4974 324.962L159.297 464.911L110.8 492.911L30 352.962L30.2486 352.818L24.2487 287L78.2488 325.105ZM58.1699 350.554L112.57 444.777L116.727 442.377L62.3275 348.154L58.1699 350.554Z" fill="#6C6247" />
+										<path d="M960.049 346.818L960.297 346.962L879.497 486.911L831 458.911L911.8 318.962L912.048 319.105L966.049 281L960.049 346.818ZM927.97 342.154L873.57 436.377L877.727 438.777L932.127 344.554L927.97 342.154Z" fill="#6C6247" />
+										<path d="M661.311 268.772L661.622 268.952L560.622 443.889L500 408.889L601 233.952L601.311 234.132L668.811 186.5L661.311 268.772ZM621.213 262.942L553.213 380.722L558.409 383.722L626.409 265.942L621.213 262.942Z" fill="#6C6247" />
+										<circle cx="500" cy="125" r="125" fill="#1C1103" />
+										<circle cx="163" cy="232" r="100" fill="#1C1103" />
+										<rect x="325" y="301" width="350" height="558" rx="92" fill="#1C1103" />
+										<rect x="23" y="369" width="280" height="440" rx="70" fill="#1C1103" />
+										<circle cx="837" cy="232" r="100" fill="#1C1103" />
+										<rect x="697" y="369" width="280" height="440" rx="70" fill="#1C1103" />
+									</svg>
+								</button>
+							)
+								:
+								<div />
 							}
-						</ul>
 
-						<form action={sendMessage} id="chat-form" >
-							<input type="text" className="border-2" id="chat-box" name="chat-msg" />
-						</form>
-					</div>
+							<button id="hide-chat-button" onClick={() => setChatToggled(!chatToggled)} >
+								<motion.svg width="1.291em" height="2em" viewBox="0 0 102 158" fill="none" xmlns="http://www.w3.org/2000/svg" animate={{ rotateZ: chatToggled ? 0 : "180deg" }} >
+									<g filter="url(#filter0_g_5_9)">
+										<path d="M8.10001 78.8107L78.8107 8.09999L92.9528 22.2421L36.3843 78.8107L92.9528 135.379L78.8107 149.521L8.10001 78.8107Z" fill="#1C1103" />
+									</g>
+									<defs>
+										<filter id="filter0_g_5_9" x="5.72205e-06" y="5.72205e-06" width="101.053" height="157.621" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+											<feFlood flood-opacity="0" result="BackgroundImageFix" />
+											<feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+											<feTurbulence type="fractalNoise" baseFrequency="0.25641024112701416 0.25641024112701416" numOctaves="3" seed="8837" />
+											<feDisplacementMap in="shape" scale="16.200000762939453" xChannelSelector="R" yChannelSelector="G" result="displacedImage" width="100%" height="100%" />
+											<feMerge result="effect1_texture_5_9">
+												<feMergeNode in="displacedImage" />
+											</feMerge>
+										</filter>
+									</defs>
+								</motion.svg>
+							</button>
+						</motion.div>
+
+						{chatToggled ?
+
+							(
+								<ul id="messages" className="border border-green-500" >
+									{
+										messages.map((msg) => (
+											<li className="message " key={Number(msg.time)}>
+												<div className="message-image">
+													<img className="rounded-full " src={msg.img} />
+												</div>
+												<div className="message-text">
+
+													<div className="message-headings">
+														<h2><b>{msg.name}</b></h2>
+
+														<h3>{new Date(Number(msg.time)).toLocaleTimeString()}</h3>
+													</div>
+													<p className="message-payload">
+														{msg.payload}
+													</p>
+												</div>
+											</li>
+										))
+									}
+								</ul>
+							)
+							:
+							<div />
+						}
+
+						{chatToggled ?
+							(
+								<form action={sendMessage} id="chat-form" >
+									<input type="text" className="border-2" id="chat-box" name="chat-msg" />
+								</form>
+
+							)
+							:
+							<div />
+						}
+					</motion.div>
 					<div id="game-container" className="border">
 						<div id="map-container" className="border">
 						</div>
